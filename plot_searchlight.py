9ff8cd6c937849a47e8f71b3073d5b33e44228ca
mrdaniblu <mr.daniblu@gmail.com>
2023-11-20 17:27:41 +0100

    add plot titles

--- plot_searchlight.py
+++ plot_searchlight.py
@@ -14,8 +14,9 @@ if __name__ in "__main__":
     anat_filename='/work/816119/InSpePosNegData/BIDS_2023E/derivatives/sub-0116/anat/sub-0116_acq-T1sequence_run-1_space-MNI152NLin2009cAsym_desc-preproc_T1w.nii.gz'
 
     best_sets = [200.0, 500.0, 1000.0]
+    titles = ["A) 200 most informative voxels", "B) 500 most informative voxels", "C) 1000 most informative voxels"]
 
-    for best in best_sets:
+    for i, best in enumerate(best_sets):
         # find the percentile that makes the cutoff for the X best voxels
         perc=100*(1-best/searchlight.scores_.size)
 
@@ -26,5 +27,5 @@ if __name__ in "__main__":
         searchlight_img = new_img_like(anat_filename, searchlight.scores_)
 
         # plot
-        fig = plot_glass_brain(searchlight_img, threshold=cut)
+        fig = plot_glass_brain(searchlight_img, threshold=cut, title=titles[i])
         fig.savefig(f"out/pos_neg_searchlight_{int(best)}.png", dpi=300)
